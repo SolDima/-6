2bbd45dbe3373f5102e5bf55ad18972997925658
Dima <dimaa17.fl@gmail.com>
2023-12-19 23:16:23 +0200

    Додав коментарі

--- Лабораторна 16/Завдання 4.cpp
+++ Лабораторна 16/Завдання 4.cpp
@@ -7,7 +7,7 @@ int main() {
 	int arr[m][m];
 	int i, j, k = 0;
 	for (i = 0; i < m; i++) {
-		for (j = 0; j < m; j++) {
+		for (j = 0; j < m; j++) {               // створення масиву
 			k++;
 			arr[i][j] = k;
 
@@ -19,13 +19,13 @@ int main() {
 
 		if (i % 2 == 0) {
 			for (j = 0; j < m ; j++) {
-				printf("[%d][%d]=%3d        ", i + 1, j + 1, arr[i][j]);
+				printf("[%d][%d]=%3d        ", i + 1, j + 1, arr[i][j]);   // вивід парних рядків
 			}
 		}
 		else {
 			for (j = m - 1; j >= 0; j--) {
 				
-				printf("[%d][%d]=%3d        ", i + 1, j + 1, arr[i][j]);
+				printf("[%d][%d]=%3d        ", i + 1, j + 1, arr[i][j]);   // вивід непарних рядків
 			}
 		}
 		printf("\n");
